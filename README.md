a46ba97f4954017e1747164f798a2b70bd903c98
Felix Colaci <felix.colaci@okta.com>
2022-06-22 15:55:58 +0200

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# spa-single-sign-on-single-logout
+# SSO and Single Logout with Auth0 and two SPAs
+
+This demonstration covers the following use cases:
+
+1. Sign into an application using Auth0 universal login (using authorization code flow)
+2. Visit a second application that get's automatically signed in (using auth0 silent authentication)
+3. SSO across tabs
+4. Single Logout. When both apps run in seperate tabs/windows and either of them get's signed out the other one will notice that and sign the user out as well.
+
+## Auth0 Configuration
+
+Make sure to set the correct values in your auth0 tenant for:
+
+- Allowed Callback URLs
+- Allowed Logout URLs
+- Allowed Web Origins
+- Allowed Origins (CORS)
+
+## App Configuration
+
+Go to each app directory and fill in the correct values into `auth_config.json`.
---
+++ app-2-loyalty/public/js/app.js
@@ -0,0 +1,152 @@
+// The Auth0 client, initialized in configureClient()
+let auth0 = null;
+
+/**
+ * Starts the authentication flow
+ */
+const login = async (targetUrl) => {
+  try {
+    console.log("Logging in", targetUrl);
+
+    const options = {
+      redirect_uri: window.location.origin,
+    };
+
+    if (targetUrl) {
+      options.appState = { targetUrl };
+    }
+
+    await auth0.loginWithRedirect(options);
+  } catch (err) {
+    console.log("Log in failed", err);
+  }
+};
+
+/**
+ * Executes the logout flow
+ */
+const logout = () => {
+  try {
+    console.log("Logging out");
+    auth0.logout({
+      returnTo: window.location.origin,
+    });
+  } catch (err) {
+    console.log("Log out failed", err);
+  }
+};
+
+/**
+ * Retrieves the auth configuration from the server
+ */
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+/**
+ * Initializes the Auth0 client
+ */
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId,
+  });
+};
+
+/**
+ * Checks to see if the user is authenticated. If so, `fn` is executed. Otherwise, the user
+ * is prompted to log in
+ * @param {*} fn The function to execute if the user is logged in
+ */
+const requireAuth = async (fn, targetUrl) => {
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    return fn();
+  }
+
+  return login(targetUrl);
+};
+
+const pollSession = async () => {
+  let result = "session is not active";
+  try {
+    await auth0.getTokenSilently({ ignoreCache: true });
+    result = "session is active";
+  } catch (error) {
+    console.log(error);
+  } finally {
+    updateUI();
+  }
+  console.log(new Date().toISOString(), result);
+
+  setTimeout(pollSession, 5000);
+};
+
+// Will run when page finishes loading
+window.onload = async () => {
+  await configureClient();
+
+  // If unable to parse the history hash, default to the root URL
+  if (!showContentFromUrl(window.location.pathname)) {
+    showContentFromUrl("/");
+    window.history.replaceState({ url: "/" }, {}, "/");
+  }
+
+  const bodyElement = document.getElementsByTagName("body")[0];
+
+  // Listen out for clicks on any hyperlink that navigates to a #/ URL
+  bodyElement.addEventListener("click", (e) => {
+    if (isRouteLink(e.target)) {
+      const url = e.target.getAttribute("href");
+
+      if (showContentFromUrl(url)) {
+        e.preventDefault();
+        window.history.pushState({ url }, {}, url);
+      }
+    }
+  });
+
+  const isAuthenticated = await auth0.isAuthenticated();
+  pollSession();
+
+  if (isAuthenticated) {
+    console.log("> User is authenticated");
+    window.history.replaceState({}, document.title, window.location.pathname);
+    updateUI();
+    return;
+  } else {
+    try {
+      await auth0.getTokenSilently();
+      updateUI();
+    } catch (error) {
+      console.log(error);
+      console.log("silent auth failed");
+    }
+  }
+
+  console.log("> User not authenticated");
+
+  const query = window.location.search;
+  const shouldParseResult = query.includes("code=") && query.includes("state=");
+
+  if (shouldParseResult) {
+    console.log("> Parsing redirect");
+    try {
+      const result = await auth0.handleRedirectCallback();
+
+      if (result.appState && result.appState.targetUrl) {
+        showContentFromUrl(result.appState.targetUrl);
+      }
+
+      console.log("Logged in!");
+    } catch (err) {
+      console.log("Error parsing redirect:", err);
+    }
+
+    window.history.replaceState({}, document.title, "/");
+  }
+
+  updateUI();
+};
